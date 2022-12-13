1d8dc0e468999c5aee0debd5bc2f328194ece942
RyanHekman <ryanhekman4@gmail.com>
2022-12-12 19:20:02 -0500

    adding textbox user input model

---
+++ HolyFit/Models/UserDataEntry.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HolyFit.Models
+{
+    public class UserDataEntry
+    {
+        [BindProperty]
+        public string user_entry { get; set; }
+    }
+}
---
+++ HolyFit/PageModels/IndexModel.cs
@@ -0,0 +1,17 @@
+﻿using HolyFit.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HolyFit.PageModels
+{
+    public class IndexModel : PageModel
+    {
+        public string user_input { get; set; }
+        public void OnGet()
+        {
+        }
+        public void OnPostSubmit(UserDataEntry entry)
+        {
+            this.user_input = string.Format("textbox name: {0}", entry.user_entry);
+        }
+    }
+}
